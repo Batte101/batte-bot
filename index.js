0444cbca52118b0590b301b50bd933998fb5794b
Batte <52135422+Batte101@users.noreply.github.com>
2021-01-14 19:33:12 +0300

    Add files via upload

--- index.js
+++ index.js
@@ -7,7 +7,11 @@ let myID = config.myID;
 let prefix = config.prefix;
 
 var msgID = String;
-var devMode = Boolean; 
+var chatID = String; 
+var replyMode = Boolean;
+//var devMode = Boolean; 
+var mute = Boolean;
+var cmnd = Boolean;
 var otpravleno = Boolean;
 
 // Запуск бота
@@ -15,8 +19,12 @@ bot.on('ready', () => {
     bot.user.setActivity('Minecraft');
     console.log('Bot is Online.');
     msgID = null;
-    devMode = false;
+    chatID = null;
+    //devMode = false;
+    mute = false;
+    cmnd = false;
     otpravleno = true;
+    replyMode = false;
     bot.generateInvite(['CREATE_INSTANT_INVITE','ADD_REACTIONS','VIEW_CHANNEL',
     'SEND_MESSAGES','SEND_TTS_MESSAGES','MANAGE_MESSAGES','EMBED_LINKS','ATTACH_FILES',
     'READ_MESSAGE_HISTORY','MENTION_EVERYONE','USE_EXTERNAL_EMOJIS','VIEW_GUILD_INSIGHTS',
@@ -25,121 +33,210 @@ bot.on('ready', () => {
     });
 });
 
-// Dev mode
-bot.on('message', msg => {    
-    if (msg.author.id == myID && msg.content.toLowerCase() == prefix + 'dev') {
-        // Включение dev mode
-        if (!devMode) {
-            msg.channel.send('-------- Начало НРП чата. --------');
-            devMode = true;
-        } else 
-        // Выключение dev mode
-        if (devMode) {
-            msg.channel.send('-------- Конец НРП чата. --------');
-            devMode = false;
-        }
+// Mute Mode
+function muteUpdate(msg) {
+    // Включение Mute Mode
+    if (!mute) {
+        msg.channel.send('-------- Начало НРП чата. --------');
+        mute = true;
+    } else 
+    // Выключение Mute Mode
+    if (mute) {
+        msg.channel.send('-------- Конец НРП чата. --------');
+        mute = false;
+    }
 
+    if (msg.guild !== null) {
+        msg.delete();
+    }
+}
+
+// Reply Mode
+function replyUpdate(msg) {
+    // Включение Reply Mode
+    if (!replyMode) {
+        msg.channel.send('Я кому-то понадобился?');
+        replyMode = true;
+    } else 
+    // Выключение Reply Mode
+    if (replyMode) {
+        replyMode = false;
+    }
+
+    if (msg.guild !== null) {
+        msg.delete();
+    }
+}
+
+// Отправка сообщения
+function sendMsg(msg, adres) {
+    if (msg.attachments.size > 0) {
+        adres.send(msg.content, msg.attachments.first());            
+    } else {
+        adres.send(msg.content);
+    }  
+}
+
+// Остановка бота (принудительная)
+function crash() {
+    msg.channel.send('Hello world!');
+}
+
+// Реакция на мои сообщения
+bot.on('message', msg => {
+    if (msg.author.id == myID) {
+
+        // Реакция на сообщения на серверах:
         if (msg.guild !== null) {
-            msg.delete();
-        }
-    } 
-})
-
-// Реакция на мои сообщения на Серверах (не ЛС)
-bot.on('message', msg => {
-    if (msg.author.id == myID && !devMode && msg.guild !== null) {
-        // Замена сообщений
-        if (msg.attachments.size > 0) {
-            msg.channel.send(msg.content, msg.attachments.first());
-            setTimeout(function(){ 
-                msg.delete();
-            }, 1000);            
-        } else {
-            if (msg.content.toLowerCase() !== prefix + 'dev'){ 
-                msg.channel.send(msg.content);
-            }
-            msg.delete();
-        }   
-
-        // Очистка чата
-        let args = msg.content.substring(prefix.length).split(" ");
-        switch(args[0]) {
-            case 'clear':
-                msg.channel.bulkDelete(100);
-                break;
-        }
-    }
-})
-
-// Уведомление в ЛС при упоминании бота
-bot.on('message', msg => {
-    if (msg.mentions.has(bot.user.id) && !msg.author.bot && !devMode) {
-        bot.users.fetch(myID).then((user) => {
-            user.send('Сервер: ' + msg.guild.name + '. Канал: #' + msg.channel.name 
-            + '. От ' + msg.author.username + ': ' + msg.content);
-        })
-    }
-})
-
-// Пересылка ЛС
-bot.on('message', msg => {
-    if (!msg.author.bot && msg.guild === null && msg.author != myID && !devMode) {
-        bot.users.fetch(myID).then((user) => {
-            user.send('ЛС от ' + msg.author.username + ': ' + msg.content + 
-            '    (' + msg.author.id + ')');
-        })
-    }
-})
-
-// Ответ из ЛС
-bot.on('message', msg => {
-    if (msg.guild === null && msg.author == myID) {
-        
-        if (msgID !== null && !msg.content.includes('~')) {
-            otpravleno = false;
-            bot.users.fetch(msgID).then((user) => {                
+            // Замена моих сообщений
+            if (!mute && !msg.content.startsWith(prefix)) { 
+                sendMsg(msg, msg.channel); 
                 if (msg.attachments.size > 0) {
-                    user.send(msg.content, msg.attachments.first());           
+                    setTimeout(function(){ 
+                        msg.delete();
+                    }, 1000);
                 } else {
-                    if (msg.content.toLowerCase() !== prefix + 'dev'){ 
-                        user.send(msg.content);
-                    }
-                } 
-                               
-            })
-            setTimeout(function(){
-                otpravleno = true; 
-                msg.channel.send('Отправлено.');
-            }, 500);               
+                    msg.delete();
+                }
+                
+            }
         }
+    
+        // Реакция на сообщения в ЛС
+        else {
+            // Ответ в ЛС
+            if (msgID !== null && !msg.content.startsWith(prefix)) {
+                otpravleno = false;
+                bot.users.fetch(msgID).then((user) => {                
+                    sendMsg(msg, user);
+                })
+                setTimeout(function(){
+                    otpravleno = true; 
+                    msg.channel.send('Отправлено.');
+                }, 500);               
+            }
 
+            // Отправление в канал
+            if (chatID !== null && !msg.content.startsWith(prefix)) {
+                otpravleno = false;
+                bot.channels.fetch(chatID).then((chat) => {                
+                    sendMsg(msg, chat);
+                })
+                setTimeout(function(){
+                    otpravleno = true; 
+                    msg.channel.send('Отправлено.');
+                }, 500);               
+            }
+        }       
+
+        // Префикс-команды
         let args = msg.content.substring(prefix.length).split(" ");
         switch(args[0]) {
-            case 'ID':
-                msgID = args[1];
-                bot.users.fetch(msgID).then((user) => {
-                    msg.channel.send('Выбран пользователь ' + user.username 
-                    + '.');
-                })
-                break;
-            case 'stop':
-                msgID = null;
-                msg.channel.send('ID обнулен.');
-                break;
+            // Очистка чата
             case 'clear':
-                msg.channel.delete();
+                if (msg.guild !== null && args[1] !== null) {
+                    msg.channel.bulkDelete(args[1]);
+                }             
                 break;
-        }        
+            // Reply Mode
+            case 'reply':
+                replyUpdate(msg);                     
+                break;
+            // Mute Mode
+            case 'mute':
+                muteUpdate(msg);                       
+                break;
+            // Выбор пользователя для отправки ЛС
+            case 'ID':
+                if (msg.guild === null) {
+                    chatID = null;
+                    msgID = args[1];
+                    bot.users.fetch(msgID).then((user) => {
+                        msg.channel.send('Выбран пользователь ' + 
+                        user.username + '.');
+                    })
+                }                
+                break;
+            // Выбор канала для прослушки и отправки
+            case 'chatID':
+                if (msg.guild === null) {
+                    msgID = null;
+                    chatID = args[1];
+                    bot.channels.fetch(chatID).then((chat) => {
+                        msg.channel.send('Выбран чат ' + chat.toString() + '.');
+                        msg.channel.send('----------------------------------');
+                    })
+                }                     
+                break;
+            // Сброс ЛС и канала
+            case 'stop':
+                if (msg.guild === null) {
+                    msgID = null;
+                    chatID = null;
+                    msg.channel.send('ID обнулен.');
+                    msg.channel.send('----------------------------------'); 
+                }                     
+                break;
+            // Краш бота (ПОНИМАЮ)
+            case 'crash':
+                if (msg.guild === null && args[1] === 'yes') {
+                    crash();
+                }                                            
+                break;
+        }
     }
 })
 
-// Очистка ЛС от бота
+// Очистка от бота
 bot.on('message', msg => {
     if (msg.guild === null && msg.author.bot && msg.content == 'Отправлено.' 
     && otpravleno) {
         setTimeout(function(){ 
             msg.delete();
         }, 3000);  
+    }
+})
+
+// Уведомление в ЛС при упоминании бота
+bot.on('message', msg => {
+    if (msg.mentions.has(bot.user.id) && !msg.author.bot) {
+        bot.users.fetch(myID).then((user) => {
+            user.send('Сервер: ' + msg.guild.name + '. Канал: #' + 
+            msg.channel.name + '. От ' + msg.author.username + ': ' + 
+            msg.content);
+        })
+    }
+})
+
+// Пересылка из ЛС бота мне
+bot.on('message', msg => {
+    if (!msg.author.bot && msg.guild === null && msg.author != myID) {
+        bot.users.fetch(myID).then((user) => {
+            user.send('ЛС от ' + msg.author.username + ': ' + msg.content 
+            + '    (' + msg.author.id + ')');
+        })
+    }
+})
+
+// Реплай при пинге (Reply Mode)
+bot.on('message', msg => {
+    if (replyMode && (msg.mentions.has(bot.user.id) || msg.mentions.has(myID)) 
+    && !msg.author.bot) {
+        msg.reply('Что?');
+    }
+})
+
+// Пересылка всего канала в ЛС, если выбран канал
+bot.on('message', msg => {
+    if (msg.channel.id == chatID && msg.author.id != bot.user.id) {
+        bot.users.fetch(myID).then((user) => {
+            if (msg.attachments.size > 0) {
+                user.send(msg.author.username + ': ' + msg.content, 
+                msg.attachments.first());         
+            } else {
+                user.send(msg.author.username + ': ' + msg.content);
+            }
+        })  
     }
 })
 
